729c1be657fcfc8704e99160ec5704fe8022c2dc
Will Naugle <will.naugle@beam.dental>
2020-02-09 18:42:37 -0500

    style about text

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const P = styled.p`
+  max-width: 80%
+  text-align: center;
+`
 
 const About = () => {
   return (
     <>
       <h1>hello!</h1>
-      <p>
+      <P>
         My name is Sarah Berry and I will be graduating with a Fashion and Retail Studies degree from The Ohio State University in 2021. Throughout my undergraduate and career experience, I have uncovered a passion for expressing my creativity through design. My portfolio demonstrates how I’ve tied visuals and articles together to reflect current design trends. My coursework, organizational strengths, creative skills, and professional experience in textile and design experience have made it clear to me that I would like to pursue graphic design long term.
-      </p>
+      </P>
     </>
   )
 }
